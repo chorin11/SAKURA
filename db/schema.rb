476b1a08edab021f7cfbfc68b904be83cd29e68d
vagrant <chorin1911@gmail.com>
2020-09-08 06:05:22 +0000

    [ADD]admin/login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_062515) do
+ActiveRecord::Schema.define(version: 2020_09_08_052830) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -19,7 +31,7 @@ ActiveRecord::Schema.define(version: 2020_09_07_062515) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
-    t.string "favorite_sweet"
+    t.string "favorite_sweets"
     t.integer "profile_image_id"
     t.string "introduction"
     t.string "is_deleted", default: "f"
