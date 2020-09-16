a1238cad2d24f5157fecd4e9872805f0d879f6fb
vagrant <chorin1911@gmail.com>
2020-09-16 07:01:33 +0000

    [ADD/seed][FIX]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  id: 1,
+  name: "dorayaki",
+  email: "1@1",
+  password: "111111"
+)
