e3cf752971feb018cd9cb24fe248e56d1226bc85
vagrant <chorin1911@gmail.com>
2020-10-03 07:15:12 +0000

    [ADD]AI

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -36,6 +36,7 @@ class PostImagesController < ApplicationController
 
   def update
     post_image = PostImage.find(params[:id])
+    post_image.score = Language.get_data(post_image_params[:introduction])
     post_image.update(post_image_params)
     redirect_to post_image_path(post_image)
   end
